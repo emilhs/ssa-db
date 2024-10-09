0cdf5ea1cc26e75b858685b7db7ceecd0736ad71
Emil Hodzic-Santor <ehodzicsantor@gmail.com>
2024-10-09 13:44:30 -0600

    jawsdb config

--- config/constants.php
+++ config/constants.php
@@ -3,16 +3,17 @@
     session_start();
     
     // Create constants to store non-repeating values
-    // Get Heroku ClearDB connection information
-    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $cleardb_server = $cleardb_url["jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"];
-    $cleardb_username = $cleardb_url["g5epois4jr3fvwjm"];
-    $cleardb_password = $cleardb_url["fknimuhilplydf54"];
-    $cleardb_db = substr($cleardb_url["i4zt2ijecbj7ykgu"],1);
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("JAWSDB_URL"));
+    print_r($cleardb_url);
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
     $active_group = 'default';
     $query_builder = TRUE;
     // Connect to DB
-    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db, 3306);
     
     //$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //Selecting Database
     
